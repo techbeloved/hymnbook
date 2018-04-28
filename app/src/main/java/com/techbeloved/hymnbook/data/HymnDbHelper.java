3f481481ad292d77fc0c2964b3f4e367a82b7030
Odife K <kennedy998@gmail.com>
2018-04-28 03:28:53 +0100

    Implemented DBHelper using third party SQLiteAssetHelper

---
+++ app/src/main/java/com/techbeloved/hymnbook/data/HymnDbHelper.java
@@ -0,0 +1,20 @@
+package com.techbeloved.hymnbook.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * Created by kennedy on 4/28/18.
+ */
+
+public class HymnDbHelper extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "hymnbook_v2.db";
+    private static final int DATABASE_VERSION = 2;
+
+    public HymnDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
