eea805683e7fbd004f2510b2c582d2d3a563650c
Odife K <kennedy998@gmail.com>
2018-04-30 22:23:32 +0100

    Implement querying the database to get the hymn content

--- app/src/main/java/com/techbeloved/hymnbook/HymnDetailActivity.java
+++ app/src/main/java/com/techbeloved/hymnbook/HymnDetailActivity.java
@@ -1,10 +1,21 @@
 package com.techbeloved.hymnbook;
 
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.webkit.WebView;
+
+import com.techbeloved.hymnbook.data.HymnContract;
 
 import org.htmlcleaner.ContentNode;
 import org.htmlcleaner.TagNode;
@@ -12,26 +23,65 @@ import org.htmlcleaner.TagNode;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HymnDetailActivity extends AppCompatActivity {
+import static com.techbeloved.hymnbook.data.HymnContract.*;
 
+public class HymnDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int LOADER_ID = 1;
     public static String hymn_tag = "hymn_number";
-    private static String css_link = "<link rel=\"stylesheet\" href=\"style.css\">";
+    private Uri mUri;
+    private WebView mDetailWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hymn_detail);
-        Intent intent = getIntent();
 
-        String webData = getResources().getString(R.string.hymn_sample2);
-        WebView webView = findViewById(R.id.detail_webview);
+        // Get uri sent by hymn list
+        mUri = getIntent().getData();
+        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        switch (id) {
+            case LOADER_ID:
+                return new CursorLoader(this, mUri, null, null, null, null);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        if (data == null || data.getCount() < 1) {
+            return;
+        }
+        String htmlBodyTemplate = getString(R.string.html_body_template);
+
+        data.moveToFirst();
+        long hymn_no = ContentUris.parseId(mUri);
+        String title = data.getString(data.getColumnIndexOrThrow(HymnEntry.COLUMN_TITLE));
+        String content = data.getString(data.getColumnIndexOrThrow(HymnEntry.COLUMN_CONTENT));
+
+        // Insert the hymn number, title and content in the html template. lol
+        String webData = String.format(htmlBodyTemplate, hymn_no, title, content);
+        mDetailWebView = findViewById(R.id.detail_webview);
+
+        // Add some css goodness
+        String css_link = getString(R.string.css_link);
         webData = css_link + webData;
 
-        webView.loadDataWithBaseURL("file:///android_asset/",
+        mDetailWebView.loadDataWithBaseURL("file:///android_asset/",
                 webData, "text/html", "UTF-8", null);
-//            webView.loadUrl("file:///android_asset/index.html");
 
     }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
 
 //    private String  buildHtml(){
 //        List<String> lines = new ArrayList<>();
