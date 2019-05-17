dd4b508f14bd6cc7a5f0c28ee689642ea53eaf8a
odifek <kennedy998@gmail.com>
2019-05-17 04:10:57 +0100

    remove some unused code

--- app/src/main/java/com/techbeloved/hymnbook/data/SharedPreferencesRepo.kt
+++ app/src/main/java/com/techbeloved/hymnbook/data/SharedPreferencesRepo.kt
@@ -1,6 +1,5 @@
 package com.techbeloved.hymnbook.data
 
-import com.techbeloved.hymnbook.data.model.CatalogStatus
 import io.reactivex.Observable
 
 interface SharedPreferencesRepo {
@@ -14,15 +13,6 @@ interface SharedPreferencesRepo {
      */
     fun setFirstStart(firstStart: Boolean)
 
-    /**
-     * Returns the ids for currently queued downloads. This can be used to check that there's a download that is pending or should be processed
-     */
-    fun currentDownloadIds(): Observable<Set<String>>
-
-    /**
-     * Used to save a download id in the shared preferences
-     */
-    fun addCurrentDownloadId(currentId: String)
 
     /**
      * Returns the saved font size used to format the text in hymn detail view
@@ -54,22 +44,5 @@ interface SharedPreferencesRepo {
      */
     fun updateMidiFilesReady(value: Boolean)
 
-    /**
-     * Checks that hymns catalog status
-     */
-
-    fun hymnsCatalogDownloadStatus(): Observable<Int>
-
-    /**
-     * Updates the hymns catalog ready preference
-     */
-    fun updateHymnsCatalogStatus(@CatalogStatus value: Int)
-
-    /**
-     * Returns the download id for hymn catalog if any or -1
-     */
-    fun hymnsCatalogDownloadId(): Observable<Long>
-
-    fun updateHymnsCatalogDownloadId(value: Long)
 
 }
--- app/src/main/java/com/techbeloved/hymnbook/data/SharedPreferencesRepoImp.kt
+++ app/src/main/java/com/techbeloved/hymnbook/data/SharedPreferencesRepoImp.kt
@@ -3,8 +3,6 @@ package com.techbeloved.hymnbook.data
 import android.content.res.Resources
 import com.f2prateek.rx.preferences2.RxSharedPreferences
 import com.techbeloved.hymnbook.R
-import com.techbeloved.hymnbook.data.model.CatalogStatus
-import com.techbeloved.hymnbook.data.model.NONE
 import io.reactivex.Observable
 
 class SharedPreferencesRepoImp(private val rxPreferences: RxSharedPreferences,
@@ -17,16 +15,6 @@ class SharedPreferencesRepoImp(private val rxPreferences: RxSharedPreferences,
     override fun setFirstStart(firstStart: Boolean) {
         val firstStartPref = rxPreferences.getBoolean(resources.getString(R.string.pref_key_first_start))
         firstStartPref.set(firstStart)
-    }
-
-    override fun currentDownloadIds(): Observable<Set<String>> {
-        val currentDownloadIds = rxPreferences.getStringSet(resources.getString(R.string.pref_key_current_download_id))
-        return currentDownloadIds.asObservable()
-    }
-
-    override fun addCurrentDownloadId(currentId: String) {
-        val currentDownloadIdsPref = rxPreferences.getStringSet(resources.getString(R.string.pref_key_current_download_id))
-        currentDownloadIdsPref.set(currentDownloadIdsPref.get().plus(currentId))
     }
 
     override fun detailFontSize(): Observable<Float> {
@@ -57,26 +45,5 @@ class SharedPreferencesRepoImp(private val rxPreferences: RxSharedPreferences,
     override fun updateMidiFilesReady(value: Boolean) {
         val midiFilesReadyPref = rxPreferences.getBoolean(resources.getString(R.string.pref_key_hymn_midi_files_ready))
         midiFilesReadyPref.set(value)
-    }
-
-
-    override fun hymnsCatalogDownloadStatus(): Observable<Int> {
-        val hymnsCatalogStatusPref = rxPreferences.getInteger(resources.getString(R.string.pref_key_hymn_catalog_download_status), NONE)
-        return hymnsCatalogStatusPref.asObservable()
-    }
-
-    override fun updateHymnsCatalogStatus(@CatalogStatus value: Int) {
-        val hymnsCatalogStatusPref = rxPreferences.getInteger(resources.getString(R.string.pref_key_hymn_catalog_download_status))
-        hymnsCatalogStatusPref.set(value)
-    }
-
-    override fun hymnsCatalogDownloadId(): Observable<Long> {
-        val hymnsCatalogDownloadIdPref = rxPreferences.getLong(resources.getString(R.string.pref_key_hymn_catalog_download_id))
-        return hymnsCatalogDownloadIdPref.asObservable()
-    }
-
-    override fun updateHymnsCatalogDownloadId(value: Long) {
-        val hymnsCatalogDownloadIdPref = rxPreferences.getLong(resources.getString(R.string.pref_key_hymn_catalog_download_id))
-        hymnsCatalogDownloadIdPref.set(value)
     }
 }
