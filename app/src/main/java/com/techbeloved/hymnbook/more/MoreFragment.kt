62ba1a416fdd4d2513f17c1c1d78278279d2e6f3
odifek <kennedy998@gmail.com>
2019-06-20 11:37:57 +0100

    add share app feature

--- app/src/main/java/com/techbeloved/hymnbook/more/MoreFragment.kt
+++ app/src/main/java/com/techbeloved/hymnbook/more/MoreFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.snackbar.Snackbar
+import com.techbeloved.hymnbook.BuildConfig
 import com.techbeloved.hymnbook.R
 import com.techbeloved.hymnbook.databinding.FragmentMoreBinding
 
@@ -30,8 +32,12 @@ class MoreFragment : Fragment() {
         }
 
         binding.textviewMoreShare.setOnClickListener {
-            Snackbar.make(binding.coordinatorLayoutMore.rootView, "Coming Soon", Snackbar.LENGTH_SHORT).show()
-
+            ShareCompat.IntentBuilder.from(requireActivity()).apply {
+                setChooserTitle(R.string.share_app)
+                setType("text/plain")
+                setText(getString(R.string.play_store_web, BuildConfig.APPLICATION_ID))
+            }
+                    .startChooser()
         }
 
         NavigationUI.setupWithNavController(binding.toolbarFragmentMore, findNavController())
