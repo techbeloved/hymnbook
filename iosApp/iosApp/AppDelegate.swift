d74d5a49f23f6ab5d26c415d96e7868918dc5021
Kennedy Odife <kennedy998@gmail.com>
2025-07-06 16:51:10 +0200

    Fix: Configure Firebase for iOS and update app display name
    This commit configures Firebase for the iOS application and updates the application's display name to "Watchman Music".
    
    Key changes:
    - Added `AppDelegate.swift` to initialize Firebase using `FirebaseApp.configure()`.
    - Updated `iOSApp.swift` to use the new `AppDelegate` via `@UIApplicationDelegateAdaptor`.
    - Modified `project.pbxproj`:
        - Included `AppDelegate.swift` in the build sources.
        - Added `GoogleService-Info.plist` to resources (this file is gitignored).
        - Added a new "Firebase Crashlytics Run Script" build phase.
        - Set `INFOPLIST_KEY_CFBundleDisplayName` to "Watchman Music" in build settings.
    - Updated `iosApp.xcscheme` to reflect changes in build actions.
    - Added `/iosApp/iosApp/GoogleService-Info.plist` to `.gitignore`.

---
+++ iosApp/iosApp/AppDelegate.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
