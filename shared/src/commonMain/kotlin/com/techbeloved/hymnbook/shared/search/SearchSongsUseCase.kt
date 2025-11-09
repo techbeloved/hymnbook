a7a52f4b64903d638a4f855334ecea4c66751379
Kennedy Odife <kennedy998@gmail.com>
2025-11-09 14:46:52 +0100

    Refactor: Improve search query handling in `SearchSongsUseCase`
    This commit refactors the `SearchSongsUseCase` to improve how search queries are processed.
    
    - The search query is now trimmed of leading/trailing whitespace.
    - Double quotes within the query are escaped to prevent FTS search errors.
    - The query is wrapped in double quotes to perform phrase matching in FTS searches.
    - The use case now returns an empty list if the search query is blank after trimming, preventing unnecessary database queries.

--- shared/src/commonMain/kotlin/com/techbeloved/hymnbook/shared/search/SearchSongsUseCase.kt
+++ shared/src/commonMain/kotlin/com/techbeloved/hymnbook/shared/search/SearchSongsUseCase.kt
@@ -10,21 +10,29 @@ internal class SearchSongsUseCase @Inject constructor(
     private val database: Database,
     private val dispatchersProvider: DispatchersProvider,
 ) {
-    suspend operator fun invoke(searchQuery: String, songbook: String?): List<SongTitle> =
-        withContext(dispatchersProvider.io()) {
+    suspend operator fun invoke(searchQuery: String, songbook: String?): List<SongTitle> {
+        val sanitizedQuery = searchQuery.trim().replace("\"", "\"\"")
+        val ftsQuery = if (sanitizedQuery.isNotBlank()) "\"$sanitizedQuery\"" else ""
+
+        return withContext(dispatchersProvider.io()) {
             // check if search query contains only digits, then use the searchSongbookEntry
-            if (searchQuery.matches(regex = "\\d+".toRegex())) {
+            if (sanitizedQuery.matches(regex = "\\d+".toRegex())) {
                 database.songEntityQueries.searchSongbookEntry(
-                    search = searchQuery,
+                    search = sanitizedQuery,
                     songbook = songbook,
                     mapper = ::SongTitle,
-                )
-            } else {
+                ).executeAsList()
+            } else if (ftsQuery.isNotEmpty()) { // Ensure we don't run an empty FTS query
                 database.songEntityQueries.searchSongs(
-                    search = searchQuery,
+                    search = ftsQuery,
                     songbook = songbook,
                     mapper = ::SongTitle,
-                )
-            }.executeAsList()
+                ).executeAsList()
+            } else {
+                // If the query is blank after trimming, return an empty list
+                emptyList()
+            }
         }
+    }
 }
+
